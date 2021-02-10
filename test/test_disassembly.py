6a9f02c4f63b7501cb5d13b063baca364c5db27c
Ammar Askar <ammar@ammaraskar.com>
2021-02-10 08:23:23 -0800

    Implement ADD2 and make tests more concise

--- test/test_disassembly.py
+++ test/test_disassembly.py
@@ -10,21 +10,20 @@ TEST_HARNESS = TEST_DIRECTORY / 'bin' / 'test_harness'
 def disassemble(bytecode):
     return subprocess.check_output([TEST_HARNESS], input=bytecode)
 
+
 class DisassemblyTest(unittest.TestCase):
-    def test_noop(self):
-        single_noop = b'\x9F\xA0'
-        disassembly = disassemble(single_noop)
-        self.assertIn(b'NOP', disassembly)
+    SINGLE_OPCODE_CASES = [
+        (b'\x9F\xA0', 'NOP'),
+        (b'\xA6\x39', 'ADD R3, R9'),
+        (b'\xA4\x52', 'ADD #0x5, R2'),
+        (b'\xA5\xF4', 'ADD2 #0xf, R4')
+    ]
 
-    def test_add(self):
-        add_r3_r9 = b'\xA6\x39'
-        disassembly = disassemble(add_r3_r9)
-        self.assertIn(b'ADD R3, R9', disassembly)
-
-    def test_add_with_immediate(self):
-        add_5_r2 = b'\xA4\x52'
-        disassembly = disassemble(add_5_r2)
-        self.assertIn(b'ADD #0x5, R2', disassembly)
+    def test_single_opcodes(self):
+        for i, (bytecode, disassembly) in enumerate(self.SINGLE_OPCODE_CASES):
+            with self.subTest(msg=disassembly):
+                actual_disassembly = disassemble(bytecode)
+                self.assertIn(disassembly.encode(), actual_disassembly)
 
 if __name__ == '__main__':
     unittest.main()
